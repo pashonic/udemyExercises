67d0df5f88bf17a8d72e14d9fcee23c6ceabba6a
Aaron Greene <pashonic@gmail.com>
2020-05-17 17:52:12 -0700

    Add exercise 7

---
+++ ToddMcLeod/Section9-exercise7/go.mod
@@ -0,0 +1,3 @@
+module github.com/pashonic/udemyGoExercises/ToddMcLeod/Section9-exercise7
+
+go 1.14
---
+++ ToddMcLeod/Section9-exercise7/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	year := 2050
+	if year == 2000 {
+		println("2000")
+	} else if year < 2000 {
+		println("your old")
+	} else {
+		println("no idea")
+	}
+}
