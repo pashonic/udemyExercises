1a1ad1b6e4a705acffe1fd8841e63b122ae3a37e
Aaron Greene <pashonic@gmail.com>
2020-05-17 17:58:53 -0700

    Exercise 9

---
+++ ToddMcLeod/Section9-exercise9/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	favSport := "basketball"
+	switch favSport {
+	case "basketball":
+		fmt.Println("boo")
+	case "football":
+		fmt.Println("awsome")
+	}
+}
