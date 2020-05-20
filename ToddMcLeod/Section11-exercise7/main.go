69d6bd083f26d9e420efac80fab111de561d35aa
Aaron Greene <pashonic@gmail.com>
2020-05-19 23:35:58 -0700

    fix

--- ToddMcLeod/Section11-exercise7/main.go
+++ ToddMcLeod/Section11-exercise7/main.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	x := [][]string{{"James", "Bond", "Shaken, not stirred"}, {"Miss", "Moneypenny", "Helloooooo, James."}}
-
 	for i, _ := range x {
 		fmt.Println("kick")
 		for j, v := range x[i] {
