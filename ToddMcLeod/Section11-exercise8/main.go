dfd30c2b1b9020beec58cd41eca68264750819f1
Aaron Greene <pashonic@gmail.com>
2020-05-20 21:47:32 -0700

    Fix

--- ToddMcLeod/Section11-exercise8/main.go
+++ ToddMcLeod/Section11-exercise8/main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	x := [][]string{{"James", "Bond", "Shaken, not stirred"}, {"Miss", "Moneypenny", "Helloooooo, James."}}
-	for i, _ := range x {
-		fmt.Println("kick")
-		for j, v := range x[i] {
-			fmt.Println(j, v)
+	m := map[string][]string{
+		"James":           []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_dr":           {"Being evil", "Ice cream", "Sunsets"},
+	}
+
+	for person, values := range m {
+		fmt.Printf("Name: %v\n", person)
+		for i, like := range values {
+			fmt.Printf("\tFor index %v there is like %v\n", i, like)
 		}
 	}
 }
