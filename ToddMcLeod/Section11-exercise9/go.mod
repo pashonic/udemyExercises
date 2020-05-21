4ff237e52e629ddc587d30dfb5e6dd63a5e12677
Aaron Greene <pashonic@gmail.com>
2020-05-20 21:53:46 -0700

    change

---
+++ ToddMcLeod/Section11-exercise9/go.mod
@@ -0,0 +1,3 @@
+module github.com/pashonic/udemyGoExercises/ToddMcLeod/Section11-exercise8
+
+go 1.14
--- ToddMcLeod/Section11-exercise9/main.go
+++ ToddMcLeod/Section11-exercise9/main.go
@@ -5,11 +5,9 @@ import "fmt"
 func main() {
 	m := map[string][]string{
 		"James":           []string{"Shaken, not stirred", "Martinis", "Women"},
-		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
-		"no_dr":           []string{"Being evil", "Ice cream", "Sunsets"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_dr":           {"Being evil", "Ice cream", "Sunsets"},
 	}
-
-	m["aaron"] = []string{"computers", "games", "kids"}
 
 	for person, values := range m {
 		fmt.Printf("Name: %v\n", person)
