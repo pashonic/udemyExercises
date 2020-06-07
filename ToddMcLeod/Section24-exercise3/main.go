479b5aff82ea3440b980364485e16efcec55f167
Aaron Greene <pashonic@gmail.com>
2020-06-06 21:11:38 -0700

    Add exercise.

---
+++ ToddMcLeod/Section24-exercise3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type customErr struct {
+	info string
+}
+
+func (ce customErr) Error() string {
+	return fmt.Sprintf("here is the error: %v", ce.info)
+}
+
+func main() {
+	c1 := customErr{
+		info: "cake time",
+	}
+
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println("foo ran -", e, "\n")
+}
